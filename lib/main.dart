c93ed2c49f588b399b225aec0b1e4a8f959c1452
Ahmad Yunus Afghoni <akughoni@gmail.com>
2020-04-14 18:21:48 +0700

    done

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:learn_pageview/slider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Landing(),
+    );
+  }
+}
+
+class Landing extends StatefulWidget {
+  @override
+  _LandingState createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  int _currentPage = 0;
+  PageController _controller = PageController();
+
+  List<Widget> _pages = [
+    SliderPage(
+        title: "Keep",
+        description:
+            "Accept cryptocurrencies and digital assets, keep thern here, or send to orthers",
+        image: "assets/1.svg"),
+    SliderPage(
+        title: "Buy",
+        description:
+            "Buy Bitcoin and cryptocurrencies with VISA and MasterVard right in the App",
+        image: "assets/2.svg"),
+    SliderPage(
+        title: "Sell",
+        description:
+            "Sell your Bitcoin cryptocurrencies or Change with orthres digital assets or flat money",
+        image: "assets/3.svg"),
+  ];
+
+  _onchanged(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          PageView.builder(
+            scrollDirection: Axis.horizontal,
+            onPageChanged: _onchanged,
+            controller: _controller,
+            itemCount: _pages.length,
+            itemBuilder: (context, int index) {
+              return _pages[index];
+            },
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List<Widget>.generate(_pages.length, (int index) {
+                    return AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        height: 10,
+                        width: (index == _currentPage) ? 30 : 10,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 5, vertical: 30),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: (index == _currentPage)
+                                ? Colors.blue
+                                : Colors.blue.withOpacity(0.5)));
+                  })),
+              InkWell(
+                onTap: () {
+                  _controller.nextPage(
+                      duration: Duration(milliseconds: 800),
+                      curve: Curves.easeInOutQuint);
+                },
+                child: AnimatedContainer(
+                  alignment: Alignment.center,
+                  duration: Duration(milliseconds: 300),
+                  height: 70,
+                  width: (_currentPage == (_pages.length - 1)) ? 200 : 75,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(35)),
+                  child: (_currentPage == (_pages.length - 1))
+                      ? Text(
+                          "Get Started",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        )
+                      : Icon(
+                          Icons.navigate_next,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/slider.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SliderPage extends StatelessWidget {
+  final String title;
+  final String description;
+  final String image;
+
+  SliderPage({this.title, this.description, this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset(
+            image,
+            width: width * 0.6,
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 80),
+            child: Text(
+              description,
+              style: TextStyle(
+                height: 1.5,
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+                letterSpacing: 0.7,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+        ],
+      ),
+    );
+  }
+}
